936c7695a7ad19b7f66e5e08ab63461c5491188d
Huseyin Caglayan <hcaglayan@hotmail.com>
2023-05-06 12:38:42 +0100

    Bitmap server

---
+++ RFC868-Server/BitmapServer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+
+namespace RFC868_Server
+{
+    internal class BitmapServer
+    {
+        const int width = 250;
+        const int height = 122;
+
+        public static async Task StartServerAsync(int port)
+        {
+            using var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Any, port));
+            server.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for client connections...");
+                var client = await server.AcceptAsync();
+                Console.WriteLine("Client connected!");
+
+                _ = Task.Run(() => HandleClient(client));
+            }
+        }
+
+        private static void HandleClient(Socket client)
+        {
+            try
+            {
+                client.Send(GetImage());
+                Console.WriteLine($"Sent back bitmap.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+
+        public static byte[] GetImage()
+        {
+            System.Drawing.Bitmap bitmap = new Bitmap(width, height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+                
+                g.DrawRectangle(Pens.Black, new Rectangle(0, 0, 11, 11));
+                g.DrawRectangle(Pens.Black, new Rectangle(0, 110, 11, 11));
+                g.DrawRectangle(Pens.Black, new Rectangle(238, 0, 11, 11));
+                g.DrawRectangle(Pens.Black, new Rectangle(238, 110, 11, 11));
+
+                g.DrawEllipse(Pens.Black, 30, 0, 80, 80);
+                g.DrawEllipse(Pens.Black, 139, 0, 80, 80);
+                g.DrawEllipse(Pens.Black, 30, 41, 80, 80);
+                g.DrawEllipse(Pens.Black, 139, 41, 80, 80);
+
+
+                Font f = new Font(new Font(FontFamily.GenericSerif, 22.5f), FontStyle.Italic);
+                g.DrawString(DateTime.Now.ToLongTimeString(), f, Brushes.Black, 1, 1, StringFormat.GenericDefault);
+                
+            }
+            var image = ToImageArray(bitmap);
+            bitmap.Save("my.bmp");
+            return image;
+        }
+
+        static private byte[] ToImageArray(Bitmap bitmap)
+        {
+            int colCount = height;
+            int rowCount = width;
+
+            int physicalColCount = (colCount % 8 == 0) ? colCount / 8 : (colCount / 8) + 1;
+            byte[] image = new byte[physicalColCount * rowCount];
+            for (int row = 0; row < rowCount; ++row)
+            {
+                int x = row;
+                int y = height - 1;
+                for (int col = 0; col < physicalColCount; ++col)
+                {
+                    for (int bit = 0; bit < 8; ++bit)
+                    {
+                        //Console.WriteLine($"{x}, {y}");
+                        bool pixelIsWhite;
+                        if (y < 0)
+                        {
+                            pixelIsWhite = true;
+                        }
+                        else
+                        {
+                            pixelIsWhite = bitmap.GetPixel(x, y).GetBrightness() > 0.8f;
+                        }
+                        if (pixelIsWhite)
+                        {
+                            image[col + physicalColCount * row] |= (byte)(0x80 >>  bit);
+                        }
+                        --y;
+                    }
+                }
+            }
+            return image;
+        }
+
+
+
+
+       
+    }
+}
--- RFC868-Server/Program.cs
+++ RFC868-Server/Program.cs
@@ -5,49 +5,14 @@ namespace RFC868_Server
 {
     class Program
     {
-        private const int port = 5001;
-
+        
         static async Task Main(string[] args)
         {
+            BitmapServer.GetImage();
             Console.WriteLine("Starting RFC 868 Time Server...");
-            await StartServerAsync();
+            Task.WaitAll(TimeServer.StartServerAsync(5001), BitmapServer.StartServerAsync(5002));
         }
 
-        private static async Task StartServerAsync()
-        {
-            using var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            server.Bind(new IPEndPoint(IPAddress.Any, port));
-            server.Listen(10);
-
-            while (true)
-            {
-                Console.WriteLine("Waiting for client connections...");
-                var client = await server.AcceptAsync();
-                Console.WriteLine("Client connected!");
-
-                _ = Task.Run(() => HandleClient(client));
-            }
-        }
-
-        private static void HandleClient(Socket client)
-        {
-            try
-            {
-                var currentTime = DateTimeOffset.UtcNow;
-                using (MemoryStream ms = new MemoryStream())
-                using (StreamWriter sw = new StreamWriter(ms))
-                {
-                    sw.Write(currentTime.ToString("u"));
-                    sw.Flush();
-                    var buffer = ms.GetBuffer();
-                    client.Send(buffer);
-                }
-                Console.WriteLine($"Sent time: {currentTime} to client.");
-            }
-            finally
-            {
-                client.Dispose();
-            }
-        }
+        
     }
 }
